573661260293381cfbbbfc21b348eac871b6def3
Tim <19328039+scorewinner@users.noreply.github.com>
2023-04-09 23:06:29 +0200

    Update furnace-send-status.lua

--- furnace-send-status.lua
+++ furnace-send-status.lua
@@ -2,8 +2,8 @@ local modem = peripheral.find("modem") or error("No modem attached", 0)
 while true do
   if modem.isOpen(11) then
     sleep(5)
-    local redstone-input = rs.getAnalogueInput("back")
-    transmit(11, 11, redstone-input)
+    local redstoneInput = rs.getAnalogueInput("back")
+    transmit(11, 11, redstoneInput)
   else
     modem.open(11)
   end
