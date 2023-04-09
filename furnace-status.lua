856e59e196dcda7ef477a7c99d0a52fc4b90c7fb
Tim <19328039+scorewinner@users.noreply.github.com>
2023-04-09 23:42:14 +0200

    Update furnace-status.lua

--- furnace-status.lua
+++ furnace-status.lua
@@ -1,16 +1,27 @@
 local modem = peripheral.find("modem") or error("No modem attached", 0)
 local monitor = peripheral.find("monitor")
+
+monitor.setTextScale(0.5)
+
 while true do
   if modem.isOpen(11) then
     sleep(5)
     local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     monitor.clear()
-    monitor.setCursorPos(1,1)
+    monitor.setCursorPos(3,1)
     monitor.write("Furnace 1")
-    monitor.setCursorPos(2,1)
+    monitor.setCursorPos(4,2)
     monitor.write("Fill Lvl")
-    monitor.setCursorPos(3,2)
+    monitor.setCursorPos(6,3)
     monitor.write(message)
+    
+    if message == 5.0 then
+      monitor.setBackgroundColor(color.orange)
+    end
+    if message == 0.0 then
+      monitor.setBackgroundColor(color.red)
+    end
+    
   else
     modem.open(11)
   end
