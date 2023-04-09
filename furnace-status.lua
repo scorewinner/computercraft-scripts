473ce279e67b59cc2753625acd98fd846c3b571d
Tim <19328039+scorewinner@users.noreply.github.com>
2023-04-09 23:00:31 +0200

    Update furnace-status.lua

--- furnace-status.lua
+++ furnace-status.lua
@@ -1,2 +1,8 @@
 local modem = peripheral.find("modem") or error("No modem attached", 0)
-modem.open(11)
+while true do
+  if modem.isOpen(11) then
+    sleep(5)
+  else
+    modem.open(11)
+  end
+end
