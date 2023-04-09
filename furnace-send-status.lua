cb4f9bd6bf1442118232e02bc24520fbf3c19fdf
Tim <19328039+scorewinner@users.noreply.github.com>
2023-04-09 23:59:51 +0200

    Update furnace-send-status.lua

--- furnace-send-status.lua
+++ furnace-send-status.lua
@@ -3,7 +3,7 @@ while true do
   if modem.isOpen(11) then
     sleep(2)
     local redstoneInput = rs.getAnalogueInput("back")
-    transmit(11, 11, redstoneInput)
+    modem.transmit(11, 11, redstoneInput)
   else
     modem.open(11)
   end
